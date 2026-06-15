47c5ab0fc4085dd6566b6429201e8a40cb9a3bd2
Gabriel Suquet <91719818+gsuquet@users.noreply.github.com>
2026-06-15 21:45:51 +0200

    feat: add initial terraform modules templates (#121)
    Signed-off-by: Gabriel Suquet <91719818+gsuquet@users.noreply.github.com>

---
+++ .mise/tasks/_base/log.sh
@@ -0,0 +1,20 @@
+
+
+# Colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+# Function to log messages
+log_info() {
+    echo -e "${GREEN}[INFO]${NC} $1"
+}
+
+log_warn() {
+    echo -e "${YELLOW}[WARN]${NC} $1"
+}
+
+log_error() {
+    echo -e "${RED}[ERROR]${NC} $1"
+}
---
+++ terraform/templates/modules/helm/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "3.1.1"
+    }
+    kubectl = {
+      source = "alekc/kubectl"
+      version = "2.2.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "3.1.0"
+    }
+  }
+}
