f31256fc6ae133dc1f93f403794cf63fe02de640
Gabriel Suquet <91719818+gsuquet@users.noreply.github.com>
2026-09-09 21:18:58 +0200

    feat(dev): enable hubble observability

--- terraform/kind-dev-01/main.tf
+++ terraform/kind-dev-01/main.tf
@@ -9,10 +9,12 @@ module "kind_cluster" {
 module "cilium" {
   source = "../modules/cilium"
 
-  cluster_name     = module.kind_cluster.name
-  cluster_type     = "kind"
-  k8s_service_host = "${module.kind_cluster.name}-control-plane"
-  k8s_service_port = "6443"
+  cluster_name         = module.kind_cluster.name
+  cluster_type         = "kind"
+  k8s_service_host     = "${module.kind_cluster.name}-control-plane"
+  k8s_service_port     = "6443"
+  hubble_relay_enabled = true
+  hubble_ui_enabled    = true
 
   depends_on = [module.kind_cluster]
 }
